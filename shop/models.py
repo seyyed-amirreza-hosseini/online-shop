b81557b10994607d0b4c5dce72174b8270ace5a1
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-08-23 16:53:19 +0330

    create product catalog

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ['title']
+        indexes = [
+            models.Index(fields=['title']),
+        ]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.title    
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    availabe = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['title']),
+            models.Index(fields=['-created']),
+        ]   
+
+    def __str__(self):
+        return self.title    
