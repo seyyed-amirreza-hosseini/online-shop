6040fad5416b7bcaabeaf29b5a6869a5c2974bdf
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-08-23 22:54:22 +0330

    build catalog views

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -14,7 +15,11 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
 
     def __str__(self):
-        return self.title    
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("shop:product_list_by_category", args=[self.slug])
+        
 
 
 class Product(models.Model):
@@ -38,3 +43,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title    
+    
+    def get_absolute_url(self):
+        return reverse("shop:product_detail", args=[self.id, self.slug])
+    
