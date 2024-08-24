470c7999c12b23e359b4919c59fa90f6ebf48567
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-08-24 13:09:29 +0330

    edit templates

--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,7 @@ from .models import Product, Category
 
 
 def product_list(request, category_slug=None):
+    category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
 
@@ -10,7 +11,7 @@ def product_list(request, category_slug=None):
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
 
-    return render(request, '/shop/product/list.html', {'category': category, 'categories': categories, 'products': products})
+    return render(request, 'shop/product/list.html', {'category': category, 'categories': categories, 'products': products})
 
 
 def product_detail(request, id, slug):
