787be87fad6d6d7e3c4e6c52cc913039855780bc
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-08-25 15:52:09 +0330

    add products to the cart

--- cart/views.py
+++ cart/views.py
@@ -12,7 +12,7 @@ def cart_add(request, product_id):
     form = CartAddProductForm(request.POST)
     
     if form.is_valid():
-        cd = form.cleaned_data()
+        cd = form.cleaned_data
         cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
 
     return redirect('cart:cart_detail')
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from cart.forms import CartAddProductForm
 from .models import Product, Category
+
 
 
 def product_list(request, category_slug=None):
@@ -16,5 +18,7 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
 
-    return render(request, 'shop/product/detail.html', {'product': product})
+    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
+
