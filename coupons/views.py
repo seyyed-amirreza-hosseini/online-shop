54da2e0d9cacddf581d3ac148a58ead7c7194ed8
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-09 15:52:35 +0330

    Apply a coupon to the shopping cart

--- coupons/views.py
+++ coupons/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.views.decorators.http import require_POST
+from .forms import CouponApplyForm
+from .models import Coupon
 
-# Create your views here.
+
+@require_POST
+def apply_coupon(request):
+    now = timezone.now()
+    form = CouponApplyForm(request.POST)
+    if form.is_valid():
+        code = form.cleaned_data.get('code')
+        try:
+            coupon = Coupon.objects.get(
+                code__iexact=code,
+                valid_from__lte=now,
+                valid_to__gte=now,
+                active=True
+            )
+            request.session['coupon_id'] = coupon.id
+        except Coupon.DoesNotExist:
+            request.session['coupon_id'] = None
+    return redirect('cart:cart_detail')
+                
--- myshop/urls.py
+++ myshop/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("cart/", include('cart.urls', namespace='cart')),
     path("orders/", include('orders.urls', namespace='orders')),
+    path('coupons/', include('coupons.urls', namespace='coupons')),
     path("", include('shop.urls', namespace='shop')),
 ]
 
